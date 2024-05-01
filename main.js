8ecfaccd820fdd31cbbb4be7fdb10b8a547918cf
KashifHanif628 <muhammadkashif628@gmail.com>
2024-05-01 16:51:34 -0700

    push my bmi calculator to github

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "weightInKg",
+        type: "number",
+        message: "Please Enter Your Weight In Kg"
+    },
+    {
+        name: "heightInMeter",
+        type: "number",
+        message: "Please Enter Your Height In Meter"
+    },
+]);
+let bmi = answer.weightInKg / (answer.heightInMeter * answer.heightInMeter);
+console.log(`Dear Kashif Your BMI is ${bmi}`);
